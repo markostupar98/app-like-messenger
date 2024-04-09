84e014a5ef1a680f5663b6db886b0d3606f71e7e
markostupar98 <markostuparets@gmail.com>
2024-04-09 22:33:46 +0200

    Adding, user screen and conversation

--- app/users/layout.tsx
+++ app/users/layout.tsx
@@ -1,13 +1,24 @@
 import Sidebar from "../components/sidebar/Sidebar";
+import getUsers from "../services/getUsers";
+import UserList from "./components/UserList";
 
-export default async function UsersLayout({
+ async function UsersLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
+  const users = await getUsers()
   return (
     <Sidebar>
-      <div className="h-full">{children}</div>
+      <div className="h-full">
+        <UserList items={users} />
+        {children}</div>
     </Sidebar>
   );
 }
+UsersLayout.getInitialProps = async () => {
+  const users = await getUsers();
+  return { users };
+};
+
+export default UsersLayout
